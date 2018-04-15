7cc80636cbde116b3ab5b35b4a4f9788d8760927
Esteban Ortiz <ematiasortiz@gmail.com>
2018-04-15 05:17:29 -0300

    tutorial end

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+
+const Single = React.createClass({
+  render() {
+    //index of the post
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
+    //get us the post
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props}/>
+        <Comments postComments = {postComments} {...this.props}/>
+      </div>
+    )
+  }
+});
+
+export default Single;
